57c986ed0aba04f51f9363e6c5310c93673fce5a
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-06 14:10:41 +0200

    [7 day] shift() method

---
+++ 7 Kyu/shortestWord.js
@@ -0,0 +1,12 @@
+/* Shortest Word 
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+// My Answer 
+
+function findShort(s){
+  let arr = s.split(' ').sort(function (a,b) {return a.length - b.length});
+    return arr.shift().length; 
+}
